ff7fb8eed692de0779a3db7a1706a75ef95e57a5
andersen_fan <as181920@hotmail.com>
2012-03-04 01:05:40 +0800

    增加header栏用户信息显示，调整message显示框

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user
+  helper_method :current_user_email
   private
   def current_user
     @current_user ||= user_from_session
+  end
+
+  def current_user_email
+    @current_user_email ||= User.find_one(_id: BSON::ObjectId(current_user))["email"]
   end
 
   def user_from_session
