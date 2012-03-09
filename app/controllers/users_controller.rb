3accccf374c445b382d6ae24866db37845f716e3
andersen_fan <as181920@hotmail.com>
2012-03-09 11:08:15 +0800

    user_home页取消翻页，增加排序－默认以note名称按字母递增排序

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
   def home
-    @notes = Note.find(owners: BSON::ObjectId(params[:id])).page(params[:page].to_i)
-    @cnt_pages=(Note.find(owners: BSON::ObjectId(params[:id])).count.to_f / 10).ceil
+    #@notes = Note.find(owners: BSON::ObjectId(params[:id])).page(params[:page].to_i)
+    #@cnt_pages=(Note.find(owners: BSON::ObjectId(params[:id])).count.to_f / 10).ceil
+    @notes = Note.find(owners: BSON::ObjectId(params[:id])).sort("name")
   end
 
   def index
