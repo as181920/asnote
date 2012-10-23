bd733b2c6b50bb3829ba3c7bfee95afd61145360
Andersen Fan <as181920@hotmail.com>
2012-10-23 16:35:40 +0800

    部署只用一个用户

--- config/deploy/online.rb
+++ config/deploy/online.rb
@@ -1,3 +1,2 @@
-set :user, 'andersen'
 set :repository,  "git@github.com:as181920/asnote.git"
 server "younoter.com", :app, :web, :db, :primary => true
--- config/deploy/vv.rb
+++ config/deploy/vv.rb
@@ -1,3 +1,2 @@
-set :user, 'bbt'
 set :repository,  "git@192.168.1.100:andersen/younoter.git"
 server "192.168.1.100", :app, :web, :db, :primary => true
